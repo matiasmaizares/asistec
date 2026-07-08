f02d2bf688b575d83d1175d63e3ba116349dcd2b
Matias Maizares <matiasmaizares2017@gmail.com>
2026-07-08 09:50:00 -0500

    chore: bootstrap Spring Boot 3 project with hexagonal architecture skeleton

---
+++ backend/src/main/java/com/innovaschools/asistec/AsistecApplication.java
@@ -0,0 +1,11 @@
+package com.innovaschools.asistec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsistecApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AsistecApplication.class, args);
+    }
+}
